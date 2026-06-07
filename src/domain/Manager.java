440139883bfccf18370efe88f2ee3442294a5e6d
Приходько Софія <sofiykapiryet@gmail.com>
2026-06-07 16:32:13 +0300

    done

--- src/domain/Manager.java
+++ src/domain/Manager.java
@@ -1,44 +1,103 @@
 package domain;
+
+/**
+ * The class Manager extends employee
+ */
 public class Manager extends Employee {
 
+    /**
+     *
+     * Manager
+     *
+     * @param employees the employees.
+     * @param name      the name.
+     * @param jobTitle  the job title.
+     * @param level     the level.
+     * @param dept      the dept.
+     * @return public
+     */
     public Manager(Employee[] employees, String name, String jobTitle, int level, String dept) {
+
         super(name, jobTitle, level, dept);
         this.employees = employees;
     }
 
     @Override
+
+    /**
+     *
+     * To string
+     *
+     * @return String
+     */
     public String toString() {
-        return super.toString()+"\nEmployees: "+getEmployees(); 
+
+        return super.toString() + "\nEmployees: " + getEmployees();
     }
 
+    /**
+     *
+     * Manager
+     *
+     * @param employees the employees.
+     * @return public
+     */
     public Manager(Employee[] employees) {
+
         super();
         this.employees = employees;
     }
-    
+
+    /**
+     *
+     * Manager
+     *
+     * @return public
+     */
     public Manager() {
+
         super();
         employees = new Employee[10];
     }
 
     private Employee[] employees;
 
+    /**
+     *
+     * Gets the employees
+     *
+     * @return the employees
+     */
     public String getEmployees() {
+
         String s = "";
         for (Employee e : employees) {
             s = s + e.getName() + ", ";
         }
-        s=s.substring(0, s.length() - 2);
+        s = s.substring(0, s.length() - 2);
         return s;
     }
 
+    /**
+     *
+     * Sets the employees
+     *
+     * @param employees the employees.
+     */
     public void setEmployees(Employee[] employees) {
-        this.employees=employees;
+
+        this.employees = employees;
     }
 
+    /**
+     *
+     * Gets the employees list
+     *
+     * @return the employees list
+     */
     public Employee[] getEmployeesList() {
+
         return employees;
     }
 
-    
 }
