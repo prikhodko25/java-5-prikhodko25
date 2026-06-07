6be0b7e957b944d9ae48da2a49c0de0ad21438fe
Приходько Софія <sofiykapiryet@gmail.com>
2026-06-07 16:28:43 +0300

    done

--- src/domain/Editor.java
+++ src/domain/Editor.java
@@ -1,44 +1,107 @@
 package domain;
+
+/**
+ * The class Editor extends artist
+ */
 public class Editor extends Artist {
 
+    /**
+     *
+     * Editor
+     *
+     * @param electronicEditing the electronic editing.
+     * @param skiils            the skiils.
+     * @param name              the name.
+     * @param jobTitle          the job title.
+     * @param level             the level.
+     * @param dept              the dept.
+     * @return public
+     */
     public Editor(boolean electronicEditing, String[] skiils, String name, String jobTitle, int level, String dept) {
+
         super(skiils, name, jobTitle, level, dept);
         this.electronicEditing = electronicEditing;
     }
 
+    /**
+     *
+     * Editor
+     *
+     * @param electronicEditing the electronic editing.
+     * @param skiils            the skiils.
+     * @return public
+     */
     public Editor(boolean electronicEditing, String[] skiils) {
+
         super(skiils);
         this.electronicEditing = electronicEditing;
     }
 
+    /**
+     *
+     * Editor
+     *
+     * @param electronicEditing the electronic editing.
+     * @return public
+     */
     public Editor(boolean electronicEditing) {
+
         super();
         this.electronicEditing = electronicEditing;
     }
-    
+
+    /**
+     *
+     * Editor
+     *
+     * @return public
+     */
     public Editor() {
+
         super();
         this.electronicEditing = true;
     }
 
     @Override
+
+    /**
+     *
+     * To string
+     *
+     * @return String
+     */
     public String toString() {
-        String s=super.toString()+"\nEditing preferences= "; 
+
+        String s = super.toString() + "\nEditing preferences= ";
         if (electronicEditing)
-            s=s+"electronic";
+            s = s + "electronic";
         else
-            s=s+"paper";
+            s = s + "paper";
         return s;
-            
+
     }
 
     private boolean electronicEditing;
 
+    /**
+     *
+     * Gets the preferences
+     *
+     * @return the preferences
+     */
     public boolean getPreferences() {
+
         return electronicEditing;
     }
 
+    /**
+     *
+     * Sets the preferences
+     *
+     * @param electronic the electronic.
+     */
     public void setPreferences(boolean electronic) {
-        this.electronicEditing=electronic;
+
+        this.electronicEditing = electronic;
     }
 }
